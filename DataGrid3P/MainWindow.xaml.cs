3bbb97234b7617e6c96fe0b33e503766adc0d72f
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2021-12-20 10:24:50 +0100

    Dodaj pliki projektów.

---
+++ DataGrid3P/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid3P
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        public ObservableCollection<Product> ListaProduktow { get; set; }
+        public ObservableCollection<string> ListaMagazynow { get; set; }
+        public string sciezka = "pack://application:,,,/Obrazki/";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            ListaProduktow = new ObservableCollection<Product>();
+            ListaProduktow.Add(new Product("aa", "komputer", 5, "Katowice_m1",new Uri(sciezka +"IMAG2063.jpg")));
+            ListaProduktow.Add(new Product("bb", "klawiatura", 50, "Katowice_m1", new Uri(sciezka + "IMAG2064.jpg")));
+            ListaProduktow.Add(new Product("cc", "monitor", 10, "Katowice_m1", new Uri(sciezka + "IMAG2065.jpg")));
+            DataContext = this;
+
+            ListaMagazynow = new ObservableCollection<string>()
+             {"Katowice_m1","Gliwice_m2", "Zabrze_m3" };
+            mag.ItemsSource = ListaMagazynow;
+            
+        }
+    }
+}
---
+++ DataGrid3P/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid3P
+{
+    public class Product
+    { public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int  LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+        public bool Dostepny { get; set; }
+
+        public Uri Zdjecie { get; set; }
+
+        public Product(string symbol, string nazwa, int liczbaSztuk, 
+            string magazyn, Uri zdjecie)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            LiczbaSztuk = liczbaSztuk;
+            Magazyn = magazyn;
+            Dostepny = true;
+            Zdjecie = zdjecie;
+        }
+    }
+}
